4d9ab9e58f3f921ea8c505776716ec1d125c44ee
michael <michael@rosewoodinitiative.org>
2017-05-22 15:38:26 -0400

    what ever

---
+++ scratchpad.js
@@ -0,0 +1,9 @@
+var firstName= "Mike";
+var lastName="Grizz";
+var fullName= firstName + ' ' + lastName;
+
+console.log("your full name is" + fullName);
+
+var yourName= prompt('what is your name?');
+if (yourName===firstName) {alert('Hey I think I know You!');}
+else{ alert("oh. Nice to meet you");}
